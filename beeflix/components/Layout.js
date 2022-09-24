803a3aa313df2840c5b061b4a1131cfdf5dd93b1
Alyyen <alyssia.colomar@gmail.com>
2022-09-24 12:00:16 +0200

    Import bootstrap

--- beeflix/components/Layout.js
+++ beeflix/components/Layout.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Head from "next/head";
+import Script from "next/script"
 
 const Layout = ({children}) => {
     return (
@@ -9,8 +10,17 @@ const Layout = ({children}) => {
                 <title>BeeFlix</title>
                 <meta name="description" content="Search movies by title" />
                 <link rel="icon" href="/favicon.png" />
+                <link
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    rel="stylesheet"
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                    crossOrigin="anonymous"
+                />
             </Head>
-
+            <Script
+                src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+                crossorigin="anonymous"/>
             <Navbar />
             {children}
             <Footer />
